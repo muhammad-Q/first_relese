ff3d7713044a757a0a393ba022f8b7c264383700
HP <taim.ms.1997@gmail.com>
2019-12-30 15:44:46 +0300

    this is the first time upload the project to the git

---
+++ app/src/main/java/com/example/firstrelese/JSONParser.java
@@ -0,0 +1,17 @@
+package com.example.firstrelese;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+public class JSONParser {
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+    // constructor
+    public JSONParser() {
+    }
+    // function get json from url
+    // by making HTTP POST or GET mehtod
+
+}
--- app/src/main/java/com/example/firstrelese/MainActivity.java
+++ app/src/main/java/com/example/firstrelese/MainActivity.java
@@ -1,67 +1,135 @@
 package com.example.firstrelese;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Configuration;
+import android.app.ProgressDialog;
 import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.AndroidException;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-
-
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Cache;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.firstrelese.ui.main.MainFragment;
 import com.example.firstrelese.ui.main.add_item;
-import com.example.firstrelese.ui.main.add_order;
 import com.example.firstrelese.ui.main.charge;
 import com.example.firstrelese.ui.main.view_item;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
 
-import static java.util.Locale.*;
+import static java.util.Locale.forLanguageTag;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressDialog pDialog;
 
+    private static String url_create_product = "http://192.168.14.137:8000/android";
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        RequestQueue requestQueue;
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
+
+            loadWeather("90210");
+
+            final TextView t=findViewById(R.id.t);
+
+
+    }
+    }
+    private void loadWeather(String zipCode) {
+        String apiUrl = "http://192.168.1.196:8000/android";
+
+        RequestQueue mRequestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+
+        // Setup the network to use the HTTPURLConnection client
+        BasicNetwork network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the request queue
+        mRequestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        mRequestQueue.start();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.POST, apiUrl, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Load the initial JSON request
+                        try {
+                          /*  JSONObject mainWeather = response.getJSONObject("main");
+                            double temperatureKelvin = mainWeather.getDouble("temp");
+                            int temperatureFahrenheit = (int) (temperatureKelvin * 9/5 - 459.67);
+
+                            TextView weatherTextView = (TextView) findViewById(R.id.t);
+                            weatherTextView.setText("The temperature is " + temperatureFahrenheit);
+
+                            Toast.makeText(getBaseContext(), "The temperature is " + temperatureFahrenheit, Toast.LENGTH_SHORT).show();
+                        */
+                        } catch (Exception e) {
+
+                            e.printStackTrace();
+                        }
+                    } },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+
+                                VolleyLog.e("Error: ", error.toString());
+                                VolleyLog.e("Error: ", error.getLocalizedMessage());
+                            }
+                        });
+
+        // Add the request to the RequestQueue
+        mRequestQueue.add(jsObjRequest);
+    }
+    public class myconnection extends AsyncTask<String,String,String>{
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+        return null;
         }
     }
 
@@ -70,7 +138,6 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, view_item.newInstance())
                     .commitNow();
-
             ListView list = (ListView) findViewById(R.id.list);
         db = new db_connection(this);
         ArrayList<String> a = db.print();
@@ -182,54 +249,54 @@ EditText driver_name=findViewById(R.id.driver_name);
             charge_cost.setError("charge cost  is Required.");
             return;
         }
-
-
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow();
     }
-
-    public void make_order(View view) {
+    public void make_order(View view) {/*
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, add_order.newInstance())
                 .commitNow();
         try {
-            java.net.URL connection_url = new java.net.URL("http://127.0.0.1:8000/login");
+            java.net.URL connection_url = new java.net.URL("uuu");
             System.out.println("Instantiated new URL: " + connection_url);
 
-
+            System.out.print("wait");
         StringRequest stringRequest = new StringRequest(Request.Method.POST, connection_url.toString(),
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        System.out.print("accept");
 
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+System.out.print("feild");
                     }
                 }){
             @Override
             protected Map<String,String> getParams(){
                 Map<String,String> params = new HashMap<String, String>();
-                params.put("subject_name","muhammad");
-                params.put("subject_nick_name","12345678");
-                params.put("group_id","11");
-
+                params.put("group_name","entervero");
 //                params.put("KEY_token", token);
+                System.out.print("wait map");
+
                 return params;
             }
 
         };
 
+            System.out.print("wait request");
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
         }
         catch (MalformedURLException e) {
             e.printStackTrace();
-        }
+        }*/
+        // creating new product in background thread
+
         }
     public void add_charge(View view)
     {
