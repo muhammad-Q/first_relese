c99206784f34c66ddf3c2128ca222c4e10607769
HP <taim.ms.1997@gmail.com>
2020-01-02 17:31:38 +0300

    this is the first time upload the project to the git

---
+++ app/src/main/java/com/example/firstrelese/entities/PostResponse.java
@@ -0,0 +1,12 @@
+package com.example.firstrelese.entities;
+
+import java.util.List;
+
+public class PostResponse {
+
+    List<Post> data;
+
+    public List<Post> getData() {
+        return data;
+    }
+}
---
+++ app/src/main/java/com/example/firstrelese/network/ApiService.java
@@ -0,0 +1,36 @@
+package com.example.firstrelese.network;
+
+import com.example.firstrelese.entities.AccessToken;
+import com.example.firstrelese.entities.PostResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    @POST("register")
+    @FormUrlEncoded
+    Call<AccessToken> register(@Field("name") String name, @Field("email") String email, @Field("password") String password);
+
+    @POST("login")
+    @FormUrlEncoded
+    Call<AccessToken> login(@Field("username") String username, @Field("password") String password);
+
+    @POST("social_auth")
+    @FormUrlEncoded
+    Call<AccessToken> socialAuth(@Field("name") String name,
+                                 @Field("email") String email,
+                                 @Field("provider") String provider,
+                                 @Field("provider_user_id") String providerUserId);
+
+    @POST("refresh")
+    @FormUrlEncoded
+    Call<AccessToken> refresh(@Field("refresh_token") String refreshToken);
+
+    @GET("posts")
+    Call<PostResponse> posts();
+
+}
